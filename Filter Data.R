58bed55cf0008c94cb8d55c14630e17f1439cdf2
stevens0027 <31221878+stevens0027@users.noreply.github.com>
2017-11-13 13:40:42 -0600

    filter data

--- Filter Data.R
+++ Filter Data.R
@@ -8,4 +8,5 @@ print(bill)
 State_description <- bill %>%
   select(Year, StateAbbr, StateDesc, CityName,PopulationCount, Category, Measure, Short_Question_Text, Data_Value_Type, Data_Value, Low_Confidence_Limit,High_Confidence_Limit)%>%
   filter(StateAbbr =="IL")%>%
-  filter(Category =="Health Outcomes")
+  filter(Category =="Health Outcomes")%>%
+  arrange(desc(PopulationCount))
